c50b087df4a96bed36ababe030f7ea3037376782
Yehor Melnyk <egorchikmelnyk@gmail.com>
2019-08-08 19:45:24 -0700

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Yehor Melnyk on 8/8/19.
+//  Copyright © 2019 Yehor Melnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
